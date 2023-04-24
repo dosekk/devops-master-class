218a30d1a0c1314bf72d59e03dfc96a4e59dfc2a
Dosek <dosekk7@hotmail.com>
2023-04-24 16:41:58 +0200

    90 udemy adding iam user

--- terraform/01-terraform-basics/main.tf
+++ terraform/01-terraform-basics/main.tf
@@ -12,9 +12,24 @@ terraform{
   resource "aws_s3_bucket" "my_S3_Bucket" {
     bucket = "my-s3-bucket-in28minutes-rali-001"
    }
+   resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc_updated"
+     
+   }
    resource "aws_s3_bucket_versioning" "versioning_example"{
     bucket = aws_s3_bucket.my_S3_Bucket.id
     versioning_configuration {
       status = "Enabled"
     }
+   }
+   output "my_sc_bucket_versioning" {
+    value = aws_s3_bucket.my_S3_Bucket.versioning[0].enabled
+   }
+
+   output "my_s3_bucket_complete_details" {
+    value = aws_s3_bucket.my_S3_Bucket
+   }
+   
+   output "my_s3_iam_user_complete_details" {
+    value = aws_iam_user.my_iam_user
    }

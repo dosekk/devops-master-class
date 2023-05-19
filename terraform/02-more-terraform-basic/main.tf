e98979c21003363098bfe19aeb6a40e56fa02809
Dosek <dosekk7@hotmail.com>
2023-05-19 15:20:30 +0200

    terraform fmt

--- terraform/02-more-terraform-basic/main.tf
+++ terraform/02-more-terraform-basic/main.tf
@@ -1,11 +1,11 @@
-terraform{
+terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.63.0"
     }
   }
 }
-  provider "aws" {
-    region = "us-east-1"
-  }
+provider "aws" {
+  region = "us-east-1"
+}
--- terraform/02-more-terraform-basic/resources.tf
+++ terraform/02-more-terraform-basic/resources.tf
@@ -1,5 +1,5 @@
-   resource "aws_iam_user" "my_iam_users" {
-    count = 3
-    name = "my_iam_user_${count.index}"
-     
-   }
+resource "aws_iam_user" "my_iam_users" {
+  count = 3
+  name  = "my_iam_user_${count.index}"
+
+}
